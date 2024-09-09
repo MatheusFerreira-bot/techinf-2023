2abf17bef3f0977b8bfeb6e56a8ebf124fb9b93d
Matheus Ferreira <matheusferr0507@gmail.com>
2024-09-09 11:11:28 -0300

    Commit Função 1 Lista 6

---
+++ JavaScript/Lista 6/func.js
@@ -0,0 +1,11 @@
+// Função 1 - csvToLines
+function csvToLines(separador,texto){
+    return texto.split(separador)
+}
+
+// Função 2 - linesToColumns
+function linesToColumns(){
+    
+}
+
+module.exports = { csvToLines, linesToColumns };
