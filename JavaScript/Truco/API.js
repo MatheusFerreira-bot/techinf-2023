9a03e207a7ac001c53e88210225c4bb98a570e35
Matheus Ferreira <matheusferr0507@gmail.com>
2024-09-23 09:55:44 -0300

    Transforma o `setDeckId` em privado

--- JavaScript/Truco/API.js
+++ JavaScript/Truco/API.js
@@ -1,39 +1,44 @@
-// Temos duas formas de se utilizar esse deckID, um definido desde o inicio com uma string, pois assim é só clicar em mostrar cartas;
 let deckID = "yomoduvqk8yk";
-// O outro definindo com um objeto vazio. O problema desse é o que o programa inicia com o objeto vazio, portanto temos que clicar primeiro em novo baralho, para depois mostrar as cartas.
-// let deckID = {};
-
 const deck = new Deck(deckID);
 
 function Deck(deckID) {
+    
     this.deckID = deckID;
     this.listOfCards = "4C,7H,AS,7D,3C,3H,3S,3D,2C,2H,2S,2D,AC,AH,AD,KC,KH,KS,KD,JC,JH,JS,JD,QC,QH,QS,QD";
 
-    // this.novoDeck = function() {
-    //     fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${this.listOfCards}`)
-    //     .then(getJsonData)
-    //     .then(function(jsonData){
-    //     deckID = jsonData.deck_id;
-    //     })
-    // }
+    const setDeckId =  (jsonData) => { 
+        this.deckID = jsonData.deck_id
+        return jsonData;
+    }
+
     this.olharCartas = function(){
-        fetch(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=3`)
+    fetch(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=3`)
         .then(getJsonData)
         .then(atualizaInfo)
         .then(puxaCartas)
         .catch(erro => alert(erro))  
     }
+
     this.embaralharCartas = function() {
         fetch(`https://deckofcardsapi.com/api/deck/${this.deckID}/shuffle/?remaining=false`)
         .then(getJsonData)  
         .then(atualizaInfo)  
         .catch(erro => alert(erro))   
     }
+
+    this.novoDeck = function() {
+        fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${this.listOfCards}`)
+        .then(getJsonData)
+        .then(atualizaInfo)
+        .then(setDeckId)
+    }
 }
+
 function getJsonData(respostaAPI){
     const json = respostaAPI.json();
     return json;
 }
+
 function atualizaInfo(jsonData){
     if (!jsonData.success){
         throw new Error("Erro ao acessar dados da API")
